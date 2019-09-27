4369d012f7f27184e5e28073474e88cb6b48171b
MogensNoertoft <55430147+MogensNoertoft@users.noreply.github.com>
2019-09-27 15:01:02 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -150,7 +150,7 @@ function test(){
 }
 
 function openBrowser(url) {
-   var target = '_blank';
+   var target = '_system';
    var options = "location=no"
    var ref = cordova.InAppBrowser.open(url, target, options);
 }
